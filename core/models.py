7503f45e1c6c37bedf0b63af5850b704cb1b9b15
ddeelskm <ddeelskm@gmail.com>
2025-04-05 00:02:52 +0300

    Создание модели банера

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+class BannerText(models.Model):
+    title = models.CharField(min_length=20, max_length=255, blank=False, verbose_name='Текст банера')
+    published = models.BooleanField(default=True, verbose_name='Опубликовать')
+    time_created = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
