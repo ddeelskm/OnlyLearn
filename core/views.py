c3eb0f61ee25f053aba7bc13fe40edf4221f63c0
ddeelskm <ddeelskm@gmail.com>
2025-04-04 22:47:46 +0300

    Статические файлы, и вывод

--- core/views.py
+++ core/views.py
@@ -3,11 +3,19 @@ from django.shortcuts import render
 
 
 # Create your views here.
-menu = ['Курсы', 'Игры', 'Английский', 'О сайте', 'Контакты', 'Вход', 'Регистрация']
+menu = [
+    {'title': 'Курсы', 'url_page': 'courses'},
+    {'title': 'Игры', 'url_page': 'games'},
+    {'title': 'Английский', 'url_page': 'english'},
+    {'title': 'О сайте', 'url_page': 'about'},
+    {'title': 'Контакты', 'url_page': 'contact'},
+    {'title': 'Вход', 'url_page': 'login'},
+    {'title': 'Регистрация', 'url_page': 'registration'},
+        ]
 
 
 def index(request):
-    return HttpResponse('index')
+    return render(request, 'core/index.html', context={'menu': menu})
 
 
 def contacts(request):
