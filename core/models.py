bfe24570ec769d2ae3031b9df84d9af55a5ebc62
ddeelskm <ddeelskm@gmail.com>
2025-04-05 21:00:10 +0300

    Исправление модели отзыва

--- core/models.py
+++ core/models.py
@@ -1,20 +1,25 @@
 from django.core.validators import MinLengthValidator
 from django.db import models
+from django.urls import reverse
 
 
 # Create your models here.
 
 class BannerText(models.Model):
-    title = models.CharField(max_length=255, blank=False, verbose_name='Текст банера')
+    title = models.CharField(max_length=255, blank=False, unique=True, verbose_name='Текст банера')
     published = models.BooleanField(default=True, verbose_name='Опубликовать')
     time_created = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
 
 
 class CustomerReviews(models.Model):
     name = models.CharField(max_length=255, blank=False, default='Аноним', verbose_name='Автор')
-    reviews = models.TextField(blank=False, min_length=MinLengthValidator(20), verbose_name='Отзыв')
+    reviews = models.TextField(blank=False, verbose_name='Отзыв', validators=[MinLengthValidator(15, 'Минимум 15 символов')])
     image = models.ImageField(upload_to='photo_reviews/', blank=True, null=True)
+    published = models.BooleanField(default=True, verbose_name='Опубликовать')
     time_created = models.DateTimeField(auto_now_add=True, verbose_name='Время написания')
 
+    class Meta:
+        ordering = ['-time_created']
 
-
+    def get_absolute_url(self):
+        return reverse('reviews', kwargs={'slug_reviews': self.id})
