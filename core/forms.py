f760667431cb1ab44a4b9aab2c6ef56fda8766cc
ddeelskm <ddeelskm@gmail.com>
2025-04-05 20:59:04 +0300

    Форма отзыва

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from core.models import BannerText
+from core.models import BannerText, CustomerReviews
 
 
 class BannerTextForm(forms.ModelForm):
@@ -10,4 +10,20 @@ class BannerTextForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'published': forms.CheckboxInput(attrs={'class': 'form-check-input'})
+        }
+
+
+class CustomerReviewsForm(forms.ModelForm):
+    class Meta:
+        model = CustomerReviews
+        fields = ['name', 'reviews', 'image', 'published']
+        labels = {
+            'image': 'Аватарка отзыва:'
+        }
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'reviews': forms.Textarea(attrs={'class': 'form-control'}),
+            'image': forms.FileInput(attrs={'class': 'form-control'}),
+            'published': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
         }
