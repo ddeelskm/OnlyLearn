653d7e0f389ebb33127a87acd0682b52f4402417
ddeelskm <ddeelskm@gmail.com>
2025-04-06 20:04:45 +0300

    Отображение формы курсов

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import FormView, CreateView
+
+from core.utils import DataMixins
+from courses.forms import CoursesForm
+from courses.models import Courses
+
 
 # Create your views here.
+class AddCourses(DataMixins, CreateView):
+    model = Courses
+    form_class = CoursesForm
+    template_name = 'courses/addcourses.html'
+    success_url = reverse_lazy('home')
+    title_page = 'Добавление курса'
