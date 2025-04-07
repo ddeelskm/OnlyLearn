50f759e23ccabf656c2de65553ae8cee518fae4d
ddeelskm <ddeelskm@gmail.com>
2025-04-07 20:33:23 +0300

    Вывод категорий, авторов, и курсы на страницу курсов

--- courses/views.py
+++ courses/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import FormView, CreateView
+from django.views.generic import FormView, CreateView, TemplateView
 
 from core.utils import DataMixins
-from courses.forms import CoursesForm
-from courses.models import Courses
+from courses.forms import CoursesForm, AuthorsForm, CategoryForm
+from courses.models import Courses, Authors, Category
 
 
 # Create your views here.
@@ -12,5 +12,33 @@ class AddCourses(DataMixins, CreateView):
     model = Courses
     form_class = CoursesForm
     template_name = 'courses/addcourses.html'
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('courses')
     title_page = 'Добавление курса'
+
+
+class AddAuthors(DataMixins, CreateView):
+    models = Authors
+    form_class = AuthorsForm
+    template_name = 'courses/addauthors.html'
+    success_url = reverse_lazy('courses')
+    title_page = 'Добавление автора'
+
+
+class AddCategory(DataMixins, CreateView):
+    model = Category
+    form_class = CategoryForm
+    template_name = 'courses/addcategory.html'
+    success_url = reverse_lazy('courses')
+    title_page = 'Добавление категории'
+
+
+class CoursesView(DataMixins, TemplateView):
+    template_name = 'courses/courses.html'
+    title_page = 'Курсы'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['authors'] = Authors.objects.all()
+        context['courses'] = Courses.objects.all()
+        context['category'] = Category.objects.all()
+        return context
