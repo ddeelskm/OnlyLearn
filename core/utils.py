4a9920c8c53abbc48748b8b516d4b249ead3ad7c
ddeelskm <ddeelskm@gmail.com>
2025-04-09 13:07:57 +0300

    удалены лишние запиши в menu

--- core/utils.py
+++ core/utils.py
@@ -1,11 +1,9 @@
 menu = [
-    {'title': 'Курсы', 'url_page': 'courses'},
-    {'title': 'Игры', 'url_page': 'games'},
-    {'title': 'Английский', 'url_page': 'english'},
-    {'title': 'О сайте', 'url_page': 'about'},
-    {'title': 'Контакты', 'url_page': 'contact'},
-    {'title': 'Вход', 'url_page': 'login'},
-    {'title': 'Регистрация', 'url_page': 'registration'},
+    {'title': 'Курсы', 'url_page': '/courses'},
+    {'title': 'Игры', 'url_page': '/games'},
+    {'title': 'Английский', 'url_page': '/english'},
+    {'title': 'О сайте', 'url_page': '/about'},
+    {'title': 'Контакты', 'url_page': '/contact'},
 ]
 
 
@@ -16,4 +14,3 @@ class DataMixins:
     def __init__(self):
         if self.title_page:
             self.extra_context['title_page'] = self.title_page
-
